be6bbc17a552c8de46a55209d02f218465c76406
Fuad0010 <fuadft@code.edu.az>
2022-03-28 17:36:38 +0400

    feat: I added do while loops

--- Task-abstract/Program.cs
+++ Task-abstract/Program.cs
@@ -7,38 +7,46 @@ namespace Task_abstract
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"1.Square\n" +
-                              $"2.Rectangular\n" +
-                              $"0.Quit\n");
+            int Key;
 
-            int Key = Convert.ToInt32(Console.ReadLine());
-
-
-            switch (Key)
+            do
             {
-                case 1:
-                    {
-                        Console.Write("Please enter the side: ");
-                        int side = Convert.ToInt32(Console.ReadLine());
-                        Square square = new Square(side);
-                        square.CalcArea();
-                    }
-                    break;
-                case 2:
-                    {
-                        Console.Write("Please enter the width: ");
-                        int width = Convert.ToInt32(Console.ReadLine());
 
-                        Console.Write("Please enter the length: ");
-                        int length = Convert.ToInt32(Console.ReadLine());
-                        Rectangular rectangular = new Rectangular(width, length);
-                        rectangular.CalcArea();
-                    }
-                    break;
-                case 0:
-                    return;
-            }
+                Console.WriteLine($"1.Square\n" +
+                                  $"2.Rectangular\n" +
+                                  $"0.Quit\n");
+
+                Key = Convert.ToInt32(Console.ReadLine());
+
+
+            } while (Key < 0 || Key > 2);
+            
+            switch (Key)
+                {
+                    case 1:
+                        {
+                            Console.Write("Please enter the side: ");
+                            int side = Convert.ToInt32(Console.ReadLine());
+                            Square square = new Square(side);
+                            square.CalcArea();
+                        }
+                        break;
+                    case 2:
+                        {
+                            Console.Write("Please enter the width: ");
+                            int width = Convert.ToInt32(Console.ReadLine());
+
+                            Console.Write("Please enter the length: ");
+                            int length = Convert.ToInt32(Console.ReadLine());
+                            Rectangular rectangular = new Rectangular(width, length);
+                            rectangular.CalcArea();
+                        }
+                        break;
+                    case 0:
+                        return;
+                }
+            
         }
-    
+
     }
 }
